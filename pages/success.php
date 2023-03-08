b00134692204c738a5629035e9b960cd6ee75bc8
SaintLynx <azr12353@gmail.com>
2023-03-08 17:15:58 +0300

    Добавление новых страниц

---
+++ pages/success.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="ru">
+<head><title>Успех!</title></head>
+<body>
+<?php if (isset($_GET['success'])): ?>
+<div class="alert alert-success">
+<p>Спасибо за ваше сообщение!</p>
+</div>
+<?php endif; ?>
+</body>
+</html>
