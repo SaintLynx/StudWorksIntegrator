2014545c8f3328fe92319ce00ffb384c75ce3eb0
SaintLynx <azr12353@gmail.com>
2023-03-11 13:41:57 +0300

    Оптимизация страниц

--- schema.sql
+++ schema.sql
@@ -4,3 +4,13 @@ CREATE DATABASE studentsworks
   DEFAULT COLLATE utf8_general_ci;
 
 USE studentsworks;
+
+CREATE TABLE studentsworkstable (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  student_name VARCHAR(128),
+  year_of_study INT,
+  group VARCHAR(64),
+  supervisor VARCHAR(128),
+  work_name TEXT NOT NULL,
+  date_creation DATETIME DEFAULT CURRENT_TIMESTAMP,
+);
