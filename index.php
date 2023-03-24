f6bf4e449dc6ab9176a9b34f949fc27d12dc31e5
SaintLynx <azr12353@gmail.com>
2023-03-24 18:35:01 +0300

    сделан шаблон index.php и подключена DB

--- index.php
+++ index.php
@@ -1,1 +1,40 @@
 <?php
+require ("functions.php");
+require ("init.php");
+
+if (!$con) {
+    $error = mysqli_connect_error();
+} else {
+    $sql = "SELECT year_of_study FROM years_of_study";
+    $result = mysqli_query($con, $sql);
+    if (!$result) {
+    $error = mysqli_error($con);
+} else {
+    $years_of_study = mysqli_fetch_all($result, MYSQLI_ASSOC);
+};
+};
+
+if (!$con) {
+    $error = mysqli_connect_error();
+} else {
+    $sql = "SELECT supervisor_name FROM supervisors";
+    $result = mysqli_query($con, $sql);
+    if (!$result) {
+    $error = mysqli_error($con);
+} else {
+    $supervisors = mysqli_fetch_all($result, MYSQLI_ASSOC);
+};
+};
+
+$main_content = include_template("maincontent.php", [
+    "years_of_study" => $years_of_study,
+    "supervisors" => $supervisors
+]);
+
+$layout_content = include_template("layout.php", [
+    "main_content" => $main_content
+]);
+
+print($layout_content);
+
+?>
---
+++ templates/layout.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+    <!--http://127.0.0.1:3000/pages/index.html-->
+    <meta charset="utf-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Philosopher:ital,wght@0,400;0,700;1,400&family=Roboto:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
+    <title>(ИСР)Интегратор Студенческих Работ</title>
+    </head>
+    <body>
+    <header class="header">
+        <h1 class="header-title">Интегратор Студенческих Работ(ИСР) </h1>
+        <p>Проект в рамках "Разработка IT-продукта" на "Цифровых кафедрах" ИТИС КФУ</p>
+        <nav class="main-nav">
+        <ul class="site-navigation">
+            <li class="site-navigation-item"><a href="/index.php">Для студентов</a></li>
+            <li class="site-navigation-item"><a href="main.html">Для преподавателей</a></li>
+        </ul>
+        </nav>
+    </header>
+    <main class="main-content"><?= $main_content; ?></main>
+    <footer class="footer">
+        <p class="dev">Закиров А., Булатов И., Салихов А., Кузнецова А. Команда №6, РИТП 4.1</p>
+        <p class="copyright">Интегратор Студенческих Работ ©2023 Все права защищены</p>
+    </footer>
+    <script src="/script.js"></script>
+    </body>
+</html>
